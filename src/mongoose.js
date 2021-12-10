48d70bfec99887dbd604db1edf2fd0a088c93453
Siva Krishna <krishna88861@gmail.com>
2021-12-10 10:44:00 +0530

    db url

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose'); 
 // const url = "mongodb://localhost:27017/exceltohtml";
-const url = "mongodb+srv://syberze-html:Kanyarashi@cluster0.atjpk.mongodb.net/exceltohtml";
+// const url = "mongodb+srv://syberze-html:Kanyarashi@cluster0.atjpk.mongodb.net/exceltohtml";
+const url = "mongodb+srv://syberzen:syberzen123@cluster0.otq1r.mongodb.net/sybervisualizer";
 
 const options = {
     useNewUrlParser: true,
